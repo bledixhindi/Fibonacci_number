71c3e07f37a09bc4c70eaac80c2c1dfe4d7d02de
bledixhindi <54402850+bledixhindi@users.noreply.github.com>
2022-03-11 12:49:20 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+
+  int i, n;
+
+  int x = 0,y = 1;
+
+  int a = x + y;
+
+  printf("Vendos numrin n: ");
+  scanf("%d", &n);
+
+  printf("Renditja: %d, %d, ", x, y);
+
+  for (i = 0; i<= n; ++i) {
+    printf("%d, ", a);
+  x=y;
+  y=a;
+    a = x + y;
+  }
+
+  return 0;
+}
+
+
